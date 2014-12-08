bc5ce2437b65e7e1b64a2572915d52de5908354a
Brian Emil Hartz <brianhartz@gmail.com>
2014-12-08 13:14:46 -0700

    cont work on final

--- final/q2/queries.js
+++ final/q2/queries.js
@@ -1,4 +1,6 @@
 
 
-db.messages.aggregate([{$unwind:"$headers.To"},
-  {$group: {_id: '$_id', To: {$addToSet: '$headers.To'}, from: 1}},])
+db.messages.aggregate({"$unwind":"$headers.To"}, {"$group": {"_id": '$_id', "To": {"$addToSet": '$headers.To'}}, {"$project":{"To":{'$To'}}})
+
+
+db.messages.aggregate({"$unwind":"$headers.To"}, {$group:{_id:'$_id', To:{$addToSet:'$headers.To'}, From:{$first:'$headers.From'}}}, {$unwind:"$To"}, {$group:{_id:{To:"$To",From:"$From"}, count:{$sum:1}}})
